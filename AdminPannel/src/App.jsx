8727ae9217de60c3b74c01696aa39e50dae72998
PRANGYA PARAMITA <prangyachoudhury56@gmail.com>
2026-04-13 18:07:39 +0530

    all work done

--- AdminPannel/src/App.jsx
+++ AdminPannel/src/App.jsx
@@ -3,6 +3,8 @@ import AdminLayout from "../Layout/AdminLayout";
 import DashBoard from "./Pages/DashBoard/DashBoard";
 import BlogPosting from "./Pages/BlogPosting/BlogPosting";
 import PropertyPosting from "./Pages/PropertyPosting/PropertyPosting";
+import ColdLeadForm from "./Pages/ColdLead/ColdLead";
+import ColdLeadTable from "./Pages/ColdLeadTable/ColdLeadTable";
 
 const App = () => {
   return (
@@ -13,6 +15,8 @@ const App = () => {
           <Route path="/" element={<DashBoard />} />
           <Route path="/blog/post"element={<BlogPosting/>}/>
           <Route path="/property/post"element={<PropertyPosting/>}/>
+          <Route path="/admin/cold-lead" element={<ColdLeadForm />} />
+          <Route path="/admin/cold-lead-table" element={<ColdLeadTable />} />
         </Route>
       </Routes>
     </BrowserRouter>
